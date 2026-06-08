21e366fae939fc2ded02121e4511be30372c72fc
Deepesh Gupta <108192513+deepeshmlgupta@users.noreply.github.com>
2026-06-08 15:52:58 +0530

    Create app.js

---
+++ Lineage-backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    CLIENT_ID: process.env.CLIENT_ID || "NOT_FOUND",
+    TEST_SECRET: process.env.TEST_SECRET || "NOT_FOUND",
+    DATABRICKS_HOST: process.env.DATABRICKS_HOST || "NOT_FOUND",
+    DATABRICKS_PATH: process.env.DATABRICKS_PATH || "NOT_FOUND"
+  });
+});
+
+const port = process.env.DATABRICKS_APP_PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
